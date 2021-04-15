2aabb149cf70a3e0a0cc5dbf561d3879939f80eb
Kashyapbd7 <61667100+kashyapbd7@users.noreply.github.com>
2021-04-14 22:14:49 -0500

    Update webapp.py
    update to original

--- webapp.py
+++ webapp.py
@@ -9,45 +9,47 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import streamlit as st
 from PIL import  Image
-import seaborn as sns
 import pickle
+import seaborn as sns
 
+st.markdown(
+        f"""
+<style>
+    .reportview-container .main .block-container{{
+        max-width: 750px;
+        padding-top: 0.5rem;
+        padding-right: 0rem;
+        padding-left: 0rem;
+        padding-bottom: 0.5rem;
+    }}
+   
+</style>
+""",
+        unsafe_allow_html=True,
+    )
 
 
 # Creating the Title and the Sub-Title
-st.write("""
-# Money Spent on the Ecommerce Site
+st.title('Money Spent Prediction on E-commerce Plateform')
+st.markdown(' ### :point_left: Check the sidebar for more details!')
+st.markdown(' #### Applying Regression using Python with streamlit!')
+st.markdown('Adjust the parameters and the total expenditure will be diaplayed below under Prediction Amount Spent By Customer as Per Input.')
 
-""")
-st.subheader('Applying Machine Learning Linear Regression using Python!')
 
-# Open and load image
-image = Image.open('amazon.jpg')
-st.image(image, caption='amazon', use_column_width = True)
 
 # Getting the Data
 df = pd.read_csv("Ecommerce Customers")
 df = df.iloc[0: , 3:]
 
-# Set sunheader
-st.subheader('Data At a Glance')
-
-#Displaying data in table
-st.dataframe(df.head())
-
-# Displaying the chart
-st.subheader('Correlation Plot of all the Numerical Vairables:')
-chart = plt.figure()
-sns.heatmap(df.corr(),annot=True)
-st.pyplot(chart)
 
 
 # Get the feature input from Users
 def get_user_input():
-    Avg_session_length = st.sidebar.slider('Avg. Session Length', 25,40,33)
-    Time_on_App = st.sidebar.slider('Time on App', 8,16,9)
-    Time_on_website = st.sidebar.slider('Time on Website', 32,40,35)
-    Membership_length = st.sidebar.slider('Length of Membership', 0.0,7.0,2.0)
+    Avg_session_length = st.slider('Avg. Session Length (In Minutes)', 25,40,33)
+    Time_on_App = st.slider('Time on App (In Minutes)', 8,16,9)
+    Time_on_website = st.slider('Time on Website (In Minutes)', 32,40,35)
+    Membership_length = st.slider('Length of Membership (In Years)', 0.0,7.0,2.0)
+
 
     # Store a dictionary into a variable
     user_data = {'Avg_session_length':Avg_session_length,
@@ -63,8 +65,9 @@ def get_user_input():
 # Store the user input into a variable
 user_input = get_user_input()
 
+
 #Setting a subheader and displaying the user input
-st.subheader('User Input:')
+st.subheader('Your Input:')
 st.write(user_input)
 
 # Separating X and y
@@ -76,13 +79,56 @@ x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.3,random_state=
 load_clf = pickle.load(open('EC_clf.pkl', 'rb'))
 
 
-# Show model metrix
-st.subheader('Model Test Accuracy:')
-st.write(metrics.explained_variance_score(y_test,load_clf.predict(x_test)))
-
 #Store models predictions in a variable
 predictions_of_c = load_clf.predict(user_input)
 
 #Set a subheader ad display result
-st.subheader('Amount Spent By Customer: ')
+st.subheader(':point_right: Prediction Yearly Amount Spent By Customer as Per Input:' )
 st.write(predictions_of_c)
+
+# Show model metrix
+st.subheader('Model Test Accuracy:')
+st.write(metrics.explained_variance_score(y_test,load_clf.predict(x_test)))
+
+
+#Displaying data in table
+st.subheader('Data At a Glance')
+st.dataframe(df.head())
+
+
+
+
+# Sidebar Creation
+# Side bar portion of code
+author_pic = Image.open('C:/Users/kd122/Desktop/New folder/penguins-heroku-master/kashyap.jpg')
+st.sidebar.image(author_pic, "Your humble app creator", use_column_width=True)
+st.sidebar.markdown("[Source Code](https://github.com/kashyapbd7)")
+st.sidebar.write("This app uses Machine Learning!")
+st.sidebar.write("It uses a Multiple regression model with Yearly amount Spent as Dependent variable.  \n\nTrained with dummy e-commerce dataset. This model was correct 98% of the \
+	time when it came to predicting how much money will customer will spent per year on the plateform")
+st.sidebar.markdown("This sidebar concept is taken from  \nS-DeFerrari's app  \n\n")
+st.sidebar.markdown(" :point_down: Scroll Down for Visualizations")
+
+
+
+# Displaying the chart
+st.sidebar.subheader("Correlation HeatMap")
+agree= st.sidebar.button('Corr Plot' )
+if agree:
+	st.markdown('### Correlation Plot of all the Numerical Vairables:')
+	chart = plt.figure()
+	sns.heatmap(df.corr(),annot=True)
+	st.pyplot(chart)
+
+
+numeric_columns = df.select_dtypes(['float64', 'float32', 'int32', 'int64']).columns
+st.sidebar.subheader("Histogram setup")
+select_box3 = st.sidebar.selectbox(label='Select Variable', options=numeric_columns)
+agree2 = st.sidebar.button('Plot')
+if agree2:
+		st.markdown(f'### Histogram of {select_box3}')
+		fig, ax = plt.subplots() #solved by add this line 
+		ax=sns.histplot(data=df,x= select_box3 )
+		st.pyplot(fig)
+
+
